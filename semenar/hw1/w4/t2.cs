3fcaa635292e0c5fa1eb8b69519a80c527a12e0e
GlebKhramkov <113765348+GlebKhramkov@users.noreply.github.com>
2022-12-28 07:19:47 +0300

    суммы четных и нечетных

---
+++ semenar/hw1/w4/t2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Lessen
+{
+    class Program
+    {
+        static void Main(string{} args)
+        {
+            uint oddNumbersCount = 0; //нечетные числа
+            uint eveNumbersCount = 0; //четные числа
+            Console.WriteLine("введите первое число")
+            int currentValue = int.Parse(Console.Readline())
+            Console.WriteLine("введите последнее число")
+            int limit = int.Parse(Console.Readline)
+            
+            while (currentValue <= limt)
+            {
+              if (currentValue%2==0)
+              {
+                eveNumbersCount++;
+              }
+              else
+              {
+                oddNumbersCount++;
+              }
+               
+               currentValue++;
+            }
+            Console.Writeline("количество нечетных чисел: " + oddNumbersCount);
+            Console.Writeline("количество четных чисел: " + eveNumbersCount);
+            Console.ReadLine();
+
+        }
+    }
+}
