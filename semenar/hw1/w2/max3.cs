52ed6de95884b787386a46602cf2b438fdae0da2
GlebKhramkov <113765348+GlebKhramkov@users.noreply.github.com>
2022-12-28 06:22:21 +0300

    program max 3

---
+++ semenar/hw1/w2/max3.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+ 
+public class Example//так как мой массив идет через arr то усложним его выводом минимальных значений
+ 
+{
+    public static void Main()
+    {
+        int[] arr = { -6, -18, 20 }; // все значения в задаче(2, 3, 7, 44, 5, 78, 22, 3, 9,)
+        Console.WriteLine("Minimum number is " + arr.Min());
+        Console.WriteLine("Maximum number is " + arr.Max());
+    }
+}
