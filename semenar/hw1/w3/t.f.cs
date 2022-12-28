f7fc0f1f77b5bbf902370de0711a8e3c6c952b33
GlebKhramkov <113765348+GlebKhramkov@users.noreply.github.com>
2022-12-28 06:39:06 +0300

    true\false

---
+++ semenar/hw1/w3/t.f.cs
@@ -0,0 +1,43 @@
+﻿namespace check1
+ 
+{
+ 
+    class Program
+ 
+    {
+ 
+        static void Main(string[] args)
+ 
+        {
+ 
+            int i;
+ 
+            Console.Write("Введите число : ");//нашёл как сделать упростить ввод числа
+ 
+            i = int.Parse(Console.ReadLine());
+ 
+            if (i % 2 == 0)
+ 
+            {
+ 
+                Console.Write("Число четное");
+ 
+                Console.Read();
+ 
+            }
+ 
+            else
+ 
+            {
+ 
+                Console.Write("ЧИсло нечетное");
+ 
+                Console.Read();
+ 
+            }
+ 
+        }
+ 
+    }
+ 
+}
