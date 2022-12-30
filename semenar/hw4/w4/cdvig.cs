81bfe70bceb87287b14ba0cdac81365484f05ad5
GlebKhramkov <113765348+GlebKhramkov@users.noreply.github.com>
2022-12-30 05:52:02 +0300

    cdvig

---
+++ semenar/hw4/w4/cdvig.cs
@@ -0,0 +1,27 @@
+﻿ class Program
+    {
+        static void Main(string[] args)
+        {
+ 
+            int[] arr = { 5, 3, 7, 4, 6,};//тут мы можем менять числа в массиве (5, 3, 7, 4, 6,/4, 6, 5, 3, 7,)
+            int k = 3;
+            int tmp;
+            int nextInd = 0;
+ 
+            Console.WriteLine(string.Join(" ", arr));
+ 
+            for (int i = 0; i < arr.Length-1; ++i)
+            {
+                nextInd += k;
+                nextInd %= arr.Length;
+ 
+                tmp = arr[nextInd];
+                arr[nextInd] = arr[0];
+                arr[0] = tmp;
+            }
+ 
+            Console.WriteLine(string.Join(" ", arr));
+ 
+        }
+        
+    }
