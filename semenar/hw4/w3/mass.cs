0158cdf9a94ca65c42ad22cd095b332c6c0417b2
GlebKhramkov <113765348+GlebKhramkov@users.noreply.github.com>
2022-12-30 05:45:59 +0300

    mass

---
+++ semenar/hw4/w3/mass.cs
@@ -0,0 +1,21 @@
+﻿namespace ConsoleApplication2
+{
+    class Laba1
+        {
+            public static void Main(string[] args)
+            {
+                int i;
+                int[] a = new int[8];
+                Console.WriteLine("Введите массив чисел: ");
+                for (i = 0; i < 8; i++)
+                {
+                    a[i] = int.Parse(Console.ReadLine());
+                }
+ 
+                for (i = 0; i < 8; i++)
+                {
+                    Console.WriteLine(a[i]);
+                }
+            }
+        }
+}
