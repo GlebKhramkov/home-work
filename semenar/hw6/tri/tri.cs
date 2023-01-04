9eff8965f3223dff954564edf0a6ab4c0a052b3e
GlebKhramkov <113765348+GlebKhramkov@users.noreply.github.com>
2023-01-04 13:52:34 +0300

    S and P

---
+++ semenar/hw6/tri/tri.cs
@@ -0,0 +1,31 @@
+﻿Console.Write("Введите значения\nВершины А - x1 = ");
+            double x1 = double.Parse(Console.ReadLine());
+            Console.Write("y1 = ");
+            double y1 = double.Parse(Console.ReadLine());
+ 
+            Console.Write("Вершины B - x2 = ");
+            double x2 = double.Parse(Console.ReadLine());
+            Console.Write("y2 = ");
+            double y2 = double.Parse(Console.ReadLine());
+ 
+            Console.Write("Вершины C - x3 = ");
+            double x3 = double.Parse(Console.ReadLine());
+            Console.Write("y3 = ");
+            double y3 = double.Parse(Console.ReadLine());
+ 
+            double S, P;
+ 
+            S = 0.5 * Math.Abs((x2 - x1) * (y3 - y1) - (x3 - x1) * (y2 - y1));
+ 
+            Console.WriteLine("Площадь треугольника равна: " + S);
+ 
+            double AB, BC, AC;
+ 
+            AB = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+            AC = Math.Sqrt(Math.Pow((x3 - x1), 2) + Math.Pow((y3 - y1), 2));
+            BC = Math.Sqrt(Math.Pow((x3 - x2), 2) + Math.Pow((y3 - y2), 2));
+ 
+            P = AB + AC + BC;
+ 
+            Console.WriteLine("Периметр треугольника: " + P);
+            Console.ReadKey();
